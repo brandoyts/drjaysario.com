434fd1997ee8cc316201af987bb051a0742e8483
brandoyts <brando.endona.dev@gmail.com>
2021-09-09 23:44:54 +0800

    edit getPdf.js

--- pages/api/getPdf.js
+++ pages/api/getPdf.js
@@ -3,22 +3,34 @@ import path from "path";
 
 export default function handler(req, res) {
     const { fileName } = req.query;
-
-    fs.readFile(
-        path.join(process.cwd(), "public", "assets", "docs", `${fileName}.pdf`),
-        (err, data) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-
-            res.setHeader("Content-Type", "application/pdf");
-            res.setHeader(
-                "Content-Disposition",
-                `attachment; filename=${fileName}.pdf`
-            );
-
-            res.send(data);
-        }
+    const filePath = path.join(
+        process.cwd(),
+        "public",
+        "assets",
+        "docs",
+        `${fileName}.pdf`
     );
+
+    // fs.readFile(filePath, (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+
+    //     res.setHeader("Content-Type", "application/pdf");
+    //     res.setHeader(
+    //         "Content-Disposition",
+    //         `attachment; filename=${fileName}.pdf`
+    //     );
+
+    //     res.send(data);
+    // });
+
+    res.writeHead(200, {
+        "Content-Type": "application/pdf",
+        "Content-Disposition": `attachmentl; filename=${fileName}.pdf`,
+    });
+
+    var readStream = fs.createReadStream(filePath);
+    readStream.pipe(res);
 }

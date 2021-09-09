88b3dfd36184ca21cd9b28eb23b93314271d6073
brandoyts <brando.endona.dev@gmail.com>
2021-09-09 22:55:42 +0800

    testing

--- pages/api/getPdf.js
+++ pages/api/getPdf.js
@@ -2,26 +2,16 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-    // const { fileName } = req.query;
-    // const filePath = path.join(
-    //     ROOT_DIR,
-    //     "public",
-    //     "assets",
-    //     "docs",
-    //     `${fileName}.pdf`
-    // );
-
-    // const fileBuffer = fs.readFileSync(filePath);
-
-    // res.setHeader("Content-Type", "application/pdf");
-    // res.send(fileBuffer);
-
-    fs.readFile(
-        path.join(process.cwd(), "public", "assets", "docs", "Gratitude.pdf"),
-        (err, data) => {
-            console.log("testsi");
-            res.setHeader("Content-Type", "application/pdf");
-            res.download(data);
-        }
+    const { fileName } = req.query;
+    const filePath = path.join(
+        process.cwd(),
+        "public",
+        "assets",
+        "docs",
+        "Gratitude.pdf"
     );
+
+    const fileBuffer = fs.readFileSync(filePath);
+
+    res.setHeader("Content-Type", "application/pdf").send(fileBuffer);
 }

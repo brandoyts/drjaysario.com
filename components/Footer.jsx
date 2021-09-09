bf16bac25d977aa097546e86d59cdc0ffe46747f
brandoyts <brando.endona.dev@gmail.com>
2021-09-09 16:42:57 +0800

    edit Footer.tsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="text-center text-white">
-            <h2 className="text-5xl bg-yellow-500">ADS GOES HERE...</h2>
+            <h2 className="text-5xl ">ADS GOES HERE...</h2>
         </div>
     );
 }
--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-    experimental: {
-        headers() {
-            return [
-                {
-                    source: "/.well-known/apple-app-site-association",
-                    headers: [
-                        { key: "content-type", value: "application/pdf" },
-                    ],
-                },
-            ];
-        },
-    },
+    reactStrictMode: true,
 };

326d1503a751ba69b0885809f651c8dd4161f862
brandoyts <brando.endona.dev@gmail.com>
2021-09-10 00:05:58 +0800

    edit getPdf.js

--- pages/api/getPdf.js
+++ pages/api/getPdf.js
@@ -1,9 +1,6 @@
 import fs from "fs";
 import path from "path";
 
-import { createReadStream } from "fs";
-import { pipeline } from "stream";
-
 export default function handler(req, res) {
     const { fileName } = req.query;
     const filePath = path.join(
@@ -14,34 +11,21 @@ export default function handler(req, res) {
         `${fileName}.pdf`
     );
 
-    // fs.readFile(filePath, (err, data) => {
-    //     if (err) {
-    //         console.log(err);
-    //         return;
-    //     }
+    fs.readFile(filePath, (err, data) => {
+        res.setHeader("Content-Type", "application/pdf");
+        res.setHeader(
+            "Content-Disposition",
+            `attachment; filename=${fileName}.pdf`
+        );
 
-    //     res.setHeader("Content-Type", "application/pdf");
-    //     res.setHeader(
-    //         "Content-Disposition",
-    //         `attachment; filename=${fileName}.pdf`
-    //     );
-
-    //     res.send(data);
-    // });
-
+        res.send(data);
+    });
     // option 2
     // res.writeHead(200, {
     //     "Content-Type": "application/pdf",
-    //     "Content-Disposition": `attachmentl; filename=${fileName}.pdf`,
+    //     "Content-Disposition": `attachment; filename=${fileName}.pdf`,
     // });
 
     // var readStream = fs.createReadStream(filePath);
     // readStream.pipe(res);
-
-    // option 3
-    res.setHeader("Content-Type", "application/pdf");
-    const fileStream = createReadStream(filePath);
-    pipeline(fileStream, res, (error) => {
-        if (error) console.error(error);
-    });
 }
